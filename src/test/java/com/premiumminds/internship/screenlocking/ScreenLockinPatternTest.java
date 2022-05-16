9e506fad53188133188a195361cc86a165efb790
pamdo-iscte <pamdo@iscte-iul.pt>
2022-05-16 11:31:39 +0100

    Initial commit

---
+++ src/test/java/com/premiumminds/internship/screenlocking/ScreenLockinPatternTest.java
@@ -0,0 +1,55 @@
+package com.premiumminds.internship.screenlocking;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+/**
+ * Created by aamado on 05-05-2022.
+ */
+@RunWith(JUnit4.class)
+public class ScreenLockinPatternTest {
+
+  /**
+   * The corresponding implementations to test.
+   *
+   * If you want, you can make others :)
+   *
+   */
+  public ScreenLockinPatternTest() {
+  };
+
+
+  @Test
+  public void ScreenLockinPatternTestFirst3Length2Test()  throws InterruptedException, ExecutionException, TimeoutException {
+    Future<Integer> count  = new ScreenLockinPattern().countPatternsFrom(3, 2);
+    Integer result = count.get(10, TimeUnit.SECONDS);
+    assertEquals(result.intValue(), 5);
+  }
+  @Test
+  public void ScreenLockinPatternTestFirst5Length2Test()  throws InterruptedException, ExecutionException, TimeoutException {
+    Future<Integer> count  = new ScreenLockinPattern().countPatternsFrom(5, 2);
+    Integer result = count.get(10, TimeUnit.SECONDS);
+    assertEquals(result.intValue(), 8);
+  }
+  @Test
+  public void ScreenLockinPatternTestFirst1Length1Test()  throws InterruptedException, ExecutionException, TimeoutException {
+    Future<Integer> count  = new ScreenLockinPattern().countPatternsFrom(1, 1);
+    Integer result = count.get(10, TimeUnit.SECONDS);
+    assertEquals(result.intValue(), 1);
+  }
+  @Test
+  public void ScreenLockinPatternTestFirst1Length2Test()  throws InterruptedException, ExecutionException, TimeoutException {
+    Future<Integer> count  = new ScreenLockinPattern().countPatternsFrom(1, 2);
+    Integer result = count.get(10, TimeUnit.SECONDS);
+    assertEquals(result.intValue(), 5);
+  }
+}
